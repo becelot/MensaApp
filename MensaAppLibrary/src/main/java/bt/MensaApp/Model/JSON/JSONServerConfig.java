bab0a936735a40c670d2f04dee4302089eaad036
Benedict Becker <benedict.becker@rwth-aachen.de>
2019-02-27 06:23:07 +0100

    Backend is now hosted on heroku

--- MensaAppLibrary/src/main/java/bt/MensaApp/Model/JSON/JSONServerConfig.java
+++ MensaAppLibrary/src/main/java/bt/MensaApp/Model/JSON/JSONServerConfig.java
@@ -5,8 +5,8 @@ package bt.MensaApp.Model.JSON;
  */
 
 public class JSONServerConfig {
-    public static final String HOST = "192.168.0.18";
-    public static final Integer PORT = 9000;
+    public static final String HOST = "infinite-bastion-24337.herokuapp.com";
+    public static final Integer PORT = 80;
     public static final String MENSA_API_HOST = "/getMensa?uni=%s";
     public static final String MENU_API_HOST = "/getMenus?uni=%s&mensa=%s";
     public static final String UNIVERSITY_API_KEY = "/getUni";
